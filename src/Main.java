1633e00911f8f1a9a7cdb55a3ce081d534852678
moeen775 <moeenahmmed2015br@gmail.com>
2023-10-16 23:38:15 +0600

    filter

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import stream.filter;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -5,12 +7,13 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        System.out.println("Before Filter : ");
-        numbers.forEach(System.out::println);
-        List<Integer> evenNumbers = numbers.stream()
-                .filter(n -> n % 2 == 0)
-                .toList();
-        System.out.println("After Filter : ");
-        evenNumbers.forEach(System.out::println);
+        System.out.print("Before Filter : ");
+        numbers.forEach(number->{
+            System.out.print(" ");
+            System.out.print(number);
+        });
+        System.out.println();
+        System.out.print("After Filter Even Number : ");
+        filter.getNumberByFilter(numbers);
     }
 }
--- src/stream/filter.java
+++ src/stream/filter.java
@@ -1,14 +1,17 @@
 package stream;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class filter {
-    List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-    List<Integer> evenNumbers = numbers.stream()
-            .filter(n -> n % 2 == 0)
-            .collect(Collectors.toList());
-     List<Integer> getNumbers = evenNumbers;
+    public static void getNumberByFilter(List<Integer> numbers) {
+        List<Integer> evenNumber = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .toList();
+        evenNumber.forEach(n -> {
+            System.out.print(" ");
+            System.out.print(n);
+        });
+    }
 }
