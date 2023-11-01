c8ed82c74ab1e1ef4191a247dd4d3f12b72475c2
moeen775 <moeenahmmed2015br@gmail.com>
2023-11-01 19:55:06 +0600

    forEach

--- src/stream/filter.java
+++ src/stream/filter.java
@@ -1,10 +1,8 @@
 package stream;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class filter {
-
     public static List<Integer> getNumberByFilter(List<Integer> numbers) {
         return numbers.stream()
                 .filter(n -> n % 2 == 0)
---
+++ src/stream/forEach.java
@@ -0,0 +1,10 @@
+package stream;
+
+import java.util.List;
+
+public class forEach {
+
+    public static void disPlay(List<Integer> integers) {
+        integers.forEach(System.out::print);
+    }
+}
